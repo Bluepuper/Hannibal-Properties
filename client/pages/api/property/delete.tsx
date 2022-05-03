8996dfcb6a279e9168997284518b95653b03a9d0
bluepuper <razhev2017@gmail.com>
2022-05-03 15:19:56 +0300

    Extension changed

--- client/pages/api/property/delete.tsx
+++
@@ -1,9 +0,0 @@
-export default async function handler(req: any, res: any) {
-    const {id} = req.body
-    const response = await fetch(`http://localhost:5000/properties/delete/${id}`, {
-        method: 'POST',
-        body: JSON.stringify({id}),
-        headers: {'Content-Type': 'application/json'}
-    })
-    res.redirect('/dashboard/properties')
-}
